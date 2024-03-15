4930783c19714c0a5f3d410c96456f21a488dca5
Suhoniak Valdyslav <vladyslavs@realo.tech>
2024-03-15 13:22:39 +0200

    add user

---
+++ src/main/java/model/User.java
@@ -0,0 +1,47 @@
+package model;
+
+public class User {
+  private String name;
+  private String email;
+  private String password;
+
+  public User(String name, String email, String password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  public void getUser() {
+    System.out.println("User");
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void printUser() {
+    System.out.println("Name: " + name);
+    System.out.println("Email: " + email);
+    System.out.println("Password: " + password);
+  }
+}
