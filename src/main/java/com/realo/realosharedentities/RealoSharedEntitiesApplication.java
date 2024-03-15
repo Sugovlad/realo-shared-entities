c505749c4463064620465bc69327410bf3f20987
Suhoniak Valdyslav <vladyslavs@realo.tech>
2024-03-15 09:57:44 +0200

    Update Readme.md initial commit

--- src/main/java/com/realo/realosharedentities/RealoSharedEntitiesApplication.java
+++ src/main/java/com/realo/realosharedentities/RealoSharedEntitiesApplication.java
@@ -1,13 +1,4 @@
 package com.realo.realosharedentities;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class RealoSharedEntitiesApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(RealoSharedEntitiesApplication.class, args);
-    }
-
 }
