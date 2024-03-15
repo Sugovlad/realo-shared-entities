9e587d0614a11fd838a233d890a4af935a820f98
Suhoniak Valdyslav <vladyslavs@realo.tech>
2024-03-15 09:57:44 +0200

    initial commit

---
+++ src/test/java/com/realo/realosharedentities/RealoSharedEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realo.realosharedentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealoSharedEntitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
